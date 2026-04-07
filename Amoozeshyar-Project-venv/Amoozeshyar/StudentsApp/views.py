d6d7c2008b49bbf1ae6ecbc84972db7edd5bcecf
Mahdi-500 <mahdi.tech1795@gmail.com>
2026-04-07 20:00:58 +0330

    student report and choosing lesson and saving it
    - added the view for student report with another function for lesson
      type states
    
    - in saving the chosen lesson the lesson requirements check is moved to
      a new function for better readability in the original view function
    
    - for choosing the lesson the method of sending avaialble classes is
      changed and improved

--- Amoozeshyar-Project-venv/Amoozeshyar/StudentsApp/views.py
+++ Amoozeshyar-Project-venv/Amoozeshyar/StudentsApp/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from LessonsApp.models import lesson_class, lesson
 from ProfessorsApp.models import Grade
 from .forms import *
-from .forms import semester as setting_semester
+from .forms import semester as set_semester
 from .models import *
 
 # Create your views here.
@@ -74,7 +74,7 @@ def student_lesson_search_view(request):
                         filter_options[key] = j
                 
                 lessons = lesson.objects.filter(**filter_options)
-                
+
                 result = []
                 temp = []
                 for i in lessons:
@@ -83,7 +83,7 @@ def student_lesson_search_view(request):
                 for i in temp:
                     for j in range(0, len(i)):
                         result.append(i[j])
-            
+
             context = {
                 "result":result, 
                 "form":form,
@@ -100,18 +100,15 @@ def student_lesson_search_view(request):
 
 @login_required(login_url=settings.LOGIN_URL)
 def choosing_lesson_form_view(request):
-
-
     if request.method == "POST":
         form_searching = StudentLessonSearchForm(data=request.POST)
-        form_choosing = ChoosingLessonForm()
 
         result = []
         temp = []
         if form_searching.is_valid():
-            
+
             student_info = student.objects.get(student_number=request.user.username)
-            semester = int(setting_semester())
+            semester = int(set_semester())
             data = {
                 "name": form_searching.cleaned_data["query_lesson_name"],
                 "code":form_searching.cleaned_data["query_lesson_code"],
@@ -136,18 +133,18 @@ def choosing_lesson_form_view(request):
                 flag = True
             else:
                 flag = False
-                choices = []
+                available_classes = {}
+                loop_couner = 1
                 for i in result:
-                    choices.append((i.id,f"نام درس: {i.lesson_code.name}   ---   نام استاد: {i.professor_name}   ---   کد درس: {i.lesson_code.code}   ---   زمان برگزاری: {i.class_day} - {i.class_end_time} تا {i.class_start_time}"))
+                    available_classes[loop_couner] = [i.id, i.lesson_code.name, i.professor_name, i.lesson_code.code, i.class_day, f"{i.class_end_time} تا {i.class_start_time}"]
+                    loop_couner += 1
 
-                form_choosing.fields["chosen_lesson"].choices = choices
-                request.session['lesson_choices'] = choices
-                request.session["semester"] = setting_semester()
+                request.session["semester"] = set_semester()
 
                 
             context = {
                 "form_searching": form_searching,
-                "form_choosing": form_choosing,
+                "available_classes":available_classes,
                 "flag":flag
             }
             return render(request, "choosing_lesson.html", context)
@@ -163,22 +160,13 @@ def choosing_lesson_form_view(request):
 @login_required(login_url=settings.LOGIN_URL)
 def saving_chosen_lesson_view(request):
     if request.method == "POST":
-        choices = request.session.get("lesson_choices")
         form = ChoosingLessonForm(data=request.POST)
-        form.fields["chosen_lesson"].choices = choices
+        form.fields["chosen_class"].choices = [(request.POST.get("chosen_class"), "the chosen class")]
 
         if form.is_valid():
             student_info = student.objects.get(student_number = request.user.username)
-            class_info = lesson_class.objects.get(id=form.cleaned_data["chosen_lesson"])
+            class_info = lesson_class.objects.get(id=form.cleaned_data["chosen_class"])
 
-            # if student_choosing_lesson.objects.filter(
-            #     student_name=student_info,
-            #     chosen_class=class_info,
-            #     semester=request.session.get("semester")
-            # ).exists():
-            #     messages.warning(request, "این درس را قبلا برداشته اید")
-            #     return redirect("academic:choosing_lesson")
-            #else:
             try:
                 student_choosing_lesson.objects.get(student_name=student_info, chosen_class=class_info, semester=request.session.get("semester"))
                 messages.warning(request, "این درس را قبلا برداشته اید")    # ! warning
@@ -192,40 +180,17 @@ def saving_chosen_lesson_view(request):
                 duplicate_flag = False
                 for i in temp:
                     if i.chosen_class.lesson_code == class_info.lesson_code:
-                        score = Grade.objects.filter(student_name=student_info, lesson_name=i.chosen_class).last()
-                        if score >= 10:
+                        mark = Grade.objects.filter(student_name=student_info, lesson_name=i.chosen_class).last().mark
+                        if mark >= 10:
                             duplicate_flag = True
 
                 if duplicate_flag:
                     messages.error(request, "این درس را قبلا برداشته اید")  # ! error
-                    return redirect("student:choosin_lesson")
-                
+                    return redirect("student:choosing_lesson")
                 
 
-                # ? checking if the student has pssed the requirements for that lesson
-                grade = ()
-                flag_passed_pishniaz = '0'
-                if class_info.lesson_code.pishniaz.all().exists():
-                    for i in class_info.lesson_code.pishniaz.all():     # ? iterates on all of the lesson pishniazes
-                        for j in student_choosing_lesson.objects.filter(student_name=student_info):     # ? finds the student
-                            if j.chosen_class.lesson_code==i:   # ? check's if this is the class we are looking for by checking the lesson code
-                                try:
-                                    student_grade = Grade.objects.get(student_name=student_info, lesson_name=j.chosen_class).score
-
-                                    if student_grade >= 10:
-                                        flag_passed_pishniaz = '1'
-                                    
-                                    grade += tuple(flag_passed_pishniaz)
-                                
-                                except Grade.DoesNotExist:
-                                    flag_passed_pishniaz = "1"
-                                    pass
-                
-                confirmation_of_passed_all_pishniazes = True
-                for i in range(0, len(grade) - 1):
-                    confirmation_of_passed_all_pishniazes &= bool(grade[i])
-                
-                if not confirmation_of_passed_all_pishniazes:
+                status = check_lesson_requirements_status(class_info, student_info)
+                if not status:
                     messages.error(request, "ابتدا باید پیش نیاز درس را قبول بشوید")    # ! error
                     return redirect("student:choosing_lesson")
                 
@@ -256,14 +221,14 @@ def saving_chosen_lesson_view(request):
                     try:
                         privious_semester_student_classes = student_choosing_lesson.objects.filter(student_name=student_info, semester=new_semester)
                         unit = 0
-                        score = 0
+                        mark = 0
                         for i in privious_semester_student_classes:
                             for j in Grade.objects.filter(student_name=student_info, lesson_name=i):
-                                if j.score >= 10:
-                                    score += j.score
+                                if j.mark >= 10:
+                                    mark += j.mark
                                     unit += j.lesson_name.lesson_code.unit
 
-                        if score / unit >= 17.00:
+                        if mark / unit >= 17.00:
                             max_unit = 24
                         
                     except student_choosing_lesson.DoesNotExist:
@@ -287,15 +252,16 @@ def saving_chosen_lesson_view(request):
                     try:
                         privious_semester_student_classes = student_choosing_lesson.objects.filter(student_name=student_info, semester=new_semester)
                         unit = 0
-                        score = 0
+                        mark = 0
                         for i in privious_semester_student_classes:
                             for j in Grade.objects.filter(student_name=student_info, lesson_name=i):
-                                if j.score >= 10:
-                                    score += j.score
+                                if j.mark >= 10:
+                                    mark += j.mark
                                     unit += j.lesson_name.lesson_code.unit
 
-                        if score / unit >= 17.00:
-                            max_unit = 24
+                        if unit != 0:
+                            if mark / unit >= 17.00:
+                                max_unit = 24
                         
                     except student_choosing_lesson.DoesNotExist:
                         pass
@@ -316,7 +282,72 @@ def saving_chosen_lesson_view(request):
 
 
 
+@login_required(login_url=settings.LOGIN_URL)
+def student_report_view(request):
+    student_info = student.objects.get(student_number = request.user.username)
+    semester_list = student_info.lessons.all().values_list("semester").distinct("semester")
+    
+    lesson_report = {}
+    semester_status = {}
+    overall_average = 0
+    overall_units = 0
+    for i in semester_list:
+        score = 0
+        total_units = 0
+        average = 0
+        lesson_name = student_choosing_lesson.objects.filter(student_name=student_info, semester=i[0])
+        for j in lesson_name:
+            try:
+                lesson_mark = j.chosen_class.grade.get(student_name=student_info).mark
+            except Grade.DoesNotExist:
+                lesson_mark = "No mark yet"
+            lesson_report.setdefault(i[0], []).append((j, lesson_mark))
 
+            # ? calculating score, credit and average
+            if lesson_mark != "No mark yet":
+                score += (lesson_mark * j.chosen_class.lesson_code.unit)
+                total_units += j.chosen_class.lesson_code.unit
+        try:
+            average = score / total_units
+        except ZeroDivisionError:
+            pass
+        overall_average += average   
+        overall_units += total_units
+
+        semester_status.setdefault(i[0], []).append((score, total_units, average))
+
+    overall_average /= len(semester_list)
+
+    lesson_type_status = student_lesson_type_status(request)
+
+    context = {
+        "lesson_report":lesson_report,
+        "semester_status":semester_status,
+        "lesson_type_status":lesson_type_status,
+        "overall_average":overall_average,
+        "overall_units":overall_units
+    }
+
+    return render(request, "student_report.html", context)
+
+
+
+def student_lesson_type_status(request) -> dict:
+    student_info = student.objects.get(student_number=request.user.username)
+    student_lesson_type_status = {
+        "اصلی":0,
+        "پایه":0,
+        "عمومی":0,
+        "تخصصی":0,
+        "اختیاری":0,
+    }
+    for i in student_info.lessons.select_related("chosen_class").all():
+        student_lesson_type_status[i.chosen_class.lesson_code.lesson_type] += i.chosen_class.lesson_code.unit
+    return student_lesson_type_status
+
+"""
+todo - these function are for validating different things before a student can choose a class
+"""
 def maximum_unit_allowed(request, student_info, class_info, max_unit) -> bool:
     try:
         student_classes = student_choosing_lesson.objects.filter(student_name=student_info, semester=semester())
@@ -333,3 +364,36 @@ def maximum_unit_allowed(request, student_info, class_info, max_unit) -> bool:
                                                             chosen_class=class_info,
                                                             semester=request.session.get("semester"))
     return False
+
+
+def check_lesson_requirements_status(class_info, student_info) -> bool:
+    student_classes = ""
+    requirements = ""
+    passed = True
+    try:
+        requirements = class_info.lesson_code.pishniaz.all()
+    except lesson.DoesNotExist:
+        return passed
+    
+    # ? if lesson has no requirements
+    if requirements == "":
+        return True
+    else:
+        for i in requirements:
+            # ? if the student had this lesson before or not
+            try:
+                student_classes = student_info.lessons.filter(chosen_class=i.code)
+            except student_choosing_lesson.DoesNotExist:
+                pass
+
+        if not student_classes:
+            return False
+        else:
+            for i in student_classes:
+                latest_lesson_status = Grade.objects.filter(student_name=student_info, lesson_name=i.chosen_class.lesson_code)
+                if latest_lesson_status.mark >= 10:
+                    passed &= True
+                else:
+                    passed &= False
+            
+    return passed

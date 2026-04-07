20d27824e6515034e7014f76dd4c346080d2df4f
Mahdi-500 <mahdi.tech1795@gmail.com>
2026-04-07 19:54:05 +0330

    added the url configuration for the student report section

--- Amoozeshyar-Project-venv/Amoozeshyar/StudentsApp/urls.py
+++ Amoozeshyar-Project-venv/Amoozeshyar/StudentsApp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("search", views.student_lesson_search_view, name="lesson_search"),
     path("choosing_lesson", views.choosing_lesson_form_view, name="choosing_lesson"),
     path("saving", views.saving_chosen_lesson_view, name="save"),
-
+    path("student_report", views.student_report_view, name="student_report"),
 ]
